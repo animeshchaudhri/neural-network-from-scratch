0c1a52fb8e204f73737e8bc6d0feb97be1c472f5
animesh <animeshchaudhri3@gmail.com>
2024-04-16 16:58:58 +0530

    Docs : neural network features

--- first.py
+++ first.py
@@ -18,3 +18,26 @@ layer1.forward(x)
 #print(layer1.output)
 layer2.forward(layer1.output)
 print(layer2.output)
+
+# step function is if your output is greater than 0.5, then it is 1, else 0
+# sigmoid function is 1/(1+e^-x) more granular than step function
+# tanh function is (e^x - e^-x)/(e^x + e^-x)
+# relu function is max(0, x)
+# softmax function is e^x/sum(e^x)
+
+
+# what is activation function ?
+# activation function is a function that is used to introduce non-linearity in the output of a neuron.
+# It is also called transfer function.
+# It is used to decide whether a neuron should be activated or not.
+# It is used to map the input between the required values.
+# It is used to introduce non-linearity in the output of a neuron.
+# It is used to make the network learn complex patterns in the data.
+# the activation function would be differnt for different layers.
+
+# what is purpose of activation function ?
+# The purpose of activation function is to introduce non-linearity in the output of a neuron.
+# mimic firing of neurons in the brain.
+
+# what is loss in neural network ?
+# loss is the error in the output of the neural network. It is the difference between the predicted output and the actual output.
